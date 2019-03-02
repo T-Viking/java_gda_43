7e778567ef537a9b3c1e96399a68c8b574535bdd
Michał <paszkiewicz.michal@gmail.com>
2019-03-02 14:57:49 +0100

    poprawki z maina

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,12 @@
-public class main {
+import java.util.Scanner;
 
+public class main {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void add(SumEquation){
+        System.out.println("Podaj liczbę a");
+        double a = double.nextLine;
+        System.out.println("Podaj liczbę b");
+        double b = double.nextLine;
+    }
 }
