bc165e89f504bb8aa18b10bbeb7864e88bca4b87
Łukasz Hebel <wookie2044@gmail.com>
2019-03-02 14:56:59 +0100

    dzielenie/mnożenie

---
+++ src/main/java/DivEquation.java
@@ -0,0 +1,9 @@
+public class DivEquation implements iCalculable {
+
+        double x = 33.64;
+        double y = 27.98;
+
+        public double calculate() {
+            return x / y;
+        }
+}
---
+++ src/main/java/MulEquation.java
@@ -0,0 +1,9 @@
+public class MulEquation implements iCalculable {
+
+    double g = 17.44;
+    double h = 87.54;
+
+    public double calculate() {
+        return g * h;
+    }
+}
