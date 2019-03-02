78731cf4672dbc93d237b555dcab704403fd7ff6
T-Viking <olafw96@gmail.com>
2019-03-02 15:10:12 +0100

    modyfikacja

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
 
 public class main {
+    public static void main(String[] args) {
+
     Scanner scanner = new Scanner(System.in);
 
-    public static void add(SumEquation){
+        System.out.println("Podaj rodzaj operacji: ");
+        String operacja = scanner.nextLine();
+        switch (operacja){
+            case ("add"):
+                SumEquation;
+                break;
+            case ("sub"):
+                SubstractEquation;
+                break;
+            case ("div"):
+                DivEquation;
+                break;
+            case ("mul"):
+                MulEquation;
+                break;
+        }
         System.out.println("Podaj liczbę a");
         double a = double.nextLine;
         System.out.println("Podaj liczbę b");
